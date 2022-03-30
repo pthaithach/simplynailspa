af8cb6aed6897667c931026c19e04719adc10476
marketer.thach <marketer.thach@gmail.com>
2022-03-30 16:16:12 +0700

    12334

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -134,7 +134,7 @@
 
 	function onScroll(event){
 	    var scrollPos = $(document).scrollTop();
-	    $('.nav a').each(function () {
+	    $('.nav>ul>li>a').each(function () {
 	        var currLink = $(this);
 	        var refElement = $(currLink.attr("href"));
 	        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
